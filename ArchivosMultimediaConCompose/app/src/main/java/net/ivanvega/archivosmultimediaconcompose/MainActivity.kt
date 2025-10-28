eedc35f92a3680a4fc3768604a649409681d5d26
Gustavo Ivan Vega Olvera <gi.vega@itsur.edu.mx>
2025-10-28 08:58:02 -0600

    Se mando llamar la camara con un objeto rememberLauncherActivityForResult Y se tomo la foto Tema: Multimedia

--- ArchivosMultimediaConCompose/app/src/main/java/net/ivanvega/archivosmultimediaconcompose/MainActivity.kt
+++ ArchivosMultimediaConCompose/app/src/main/java/net/ivanvega/archivosmultimediaconcompose/MainActivity.kt
@@ -1,17 +1,31 @@
 package net.ivanvega.archivosmultimediaconcompose
 
+import android.net.Uri
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import net.ivanvega.archivosmultimediaconcompose.providers.MiFileProviderMedia
 import net.ivanvega.archivosmultimediaconcompose.ui.theme.ArchivosMultimediaConComposeTheme
+import kotlin.contracts.contract
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,15 +34,54 @@ class MainActivity : ComponentActivity() {
         setContent {
             ArchivosMultimediaConComposeTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    pickphosAndImage(modifier = Modifier.padding(innerPadding))
                 }
             }
         }
     }
 }
+
+@Composable
+fun pickphosAndImage (modifier: Modifier = Modifier){
+
+    // 1
+    var hasImage by remember {
+        mutableStateOf(false)
+    }
+    var hasVideo by remember {
+        mutableStateOf(false)
+    }
+    // 2
+    var imageUri by remember {
+        mutableStateOf<Uri?>(null)
+    }
+
+
+    var uri : Uri? = null
+    val ctx = LocalContext.current
+
+    val camara = rememberLauncherForActivityResult(
+        ActivityResultContracts.TakePicture()) { success ->
+        if (success) {
+            Log.d("FOTOX", "Imagen capturada con exito")
+            imageUri = uri;
+        }
+    }
+
+    Column(modifier = modifier) {
+        Button(onClick = {
+            uri = MiFileProviderMedia.getImageUri(ctx)
+            camara.launch(uri)
+
+
+        }){
+            Text(text = stringResource(id=R.string.pick_image_button))
+        }
+
+    }
+
+}
+
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
